9e024920736f954e3bb7d32097b85c3fda62f42a
shivang0101 <42436941+shivang0101@users.noreply.github.com>
2021-04-06 21:42:38 +0530

    Pre-Deployment Commit

---
+++ build/precache-manifest.ba20d4be882f4f8824ba4d96e3b0f3a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f5fe5498a5a47a0ba995de71512473",
+    "url": "/Fleo_SpaceX/index.html"
+  },
+  {
+    "revision": "f0abf219457fc2e343c6",
+    "url": "/Fleo_SpaceX/static/css/2.f6d03aaa.chunk.css"
+  },
+  {
+    "revision": "f55c2d7c3ea0eb2f2f4d",
+    "url": "/Fleo_SpaceX/static/css/main.7876cc36.chunk.css"
+  },
+  {
+    "revision": "f0abf219457fc2e343c6",
+    "url": "/Fleo_SpaceX/static/js/2.f845092f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Fleo_SpaceX/static/js/2.f845092f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55c2d7c3ea0eb2f2f4d",
+    "url": "/Fleo_SpaceX/static/js/main.c65dfd68.chunk.js"
+  },
+  {
+    "revision": "1e06f56e9243d9250c8d",
+    "url": "/Fleo_SpaceX/static/js/runtime-main.48c088f2.js"
+  },
+  {
+    "revision": "0ff414b4e68ef3f82a7fc8294064fafb",
+    "url": "/Fleo_SpaceX/static/media/rocket.0ff414b4.gif"
+  }
+]);
